d9fe536669c6798ee6c609decdbdaf83cff58035
ulyweb <uly@ulynetwork.com>
2025-06-14 11:21:29 -0700

    Update step1.sh

--- scripts/glenn/step1.sh
+++ scripts/glenn/step1.sh
@@ -1,7 +1,7 @@
 mkdir -p /mnt/ncdata
 mkdir -p /home/$USER/nextcloud-aio/
 cd /home/$USER/nextcloud-aio/
-wget -O .env https://github.com/immich-app/immich/releases/latest/download/example.env
+wget -O compose.yaml https://github.com/ulyweb/proxmox/releases/download/glenn-v1.0.0/compose.yaml
 mkdir -p /home/$USER/immich-app/
 cd /home/$USER/immich-app/
 wget -O docker-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
